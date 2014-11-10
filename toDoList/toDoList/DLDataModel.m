8899d0d9349bf696efecde2713a4cb66f73a118e
Davis Gossage <dcg13@duke.edu>
2014-11-10 15:34:48 -0500

    Added data model methods to store and fetch list from user defaults, and add items to the list

--- toDoList/toDoList/DLDataModel.m
+++ toDoList/toDoList/DLDataModel.m
@@ -7,16 +7,55 @@
 //
 
 #import "DLDataModel.h"
+#import "DLItem.h"
+
+#define USER_DEFAULT_STORE_KEY @"mainList"
 
 @implementation DLDataModel
 
 static DLDataModel* gSharedInstance = nil;
+NSMutableArray *_currentList;
 
 +(DLDataModel*)sharedInstance {
     if (!gSharedInstance) {
         gSharedInstance = [[DLDataModel alloc] init];
     }
     return gSharedInstance;
+}
+
+-(NSMutableArray *)currentList{
+    if (!_currentList){
+        [self fetchList];
+    }
+    return _currentList;
+}
+
+-(void)addItemToList:(DLItem *)item{
+    [[self currentList] addObject:item];
+    [self synchronizeList];
+}
+
+#pragma mark UserDefault interface
+
+/**
+ * synchronizes the local list with the list stored on the device
+ **/
+-(void)synchronizeList{
+    NSData *listData = [NSKeyedArchiver archivedDataWithRootObject:_currentList];
+    [[NSUserDefaults standardUserDefaults] setObject:listData forKey:USER_DEFAULT_STORE_KEY];
+    [[NSUserDefaults standardUserDefaults] synchronize];
+}
+
+/**
+ * populates the local list with the list stored on the device
+ **/
+-(void)fetchList{
+    NSData *dataFromUserDefaults = [[NSUserDefaults standardUserDefaults] objectForKey:USER_DEFAULT_STORE_KEY];
+    NSArray *arrayFromUserDefaults = [NSKeyedUnarchiver unarchiveObjectWithData:dataFromUserDefaults];
+    if (!arrayFromUserDefaults){
+        arrayFromUserDefaults = [NSArray new];
+    }
+    _currentList = [NSMutableArray arrayWithArray:arrayFromUserDefaults];
 }
 
 @end
