1b6b150bca258d6cba0caee160aa2cb4a1280e01
Davis Gossage <dcg13@duke.edu>
2014-11-10 14:41:30 -0500

    Empty single view project

---
+++ toDoList/toDoList/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  toDoList
+//
+//  Created by Davis Gossage on 11/10/14.
+//  Copyright (c) 2014 Davis Gossage. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ toDoList/toDoList/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  toDoList
+//
+//  Created by Davis Gossage on 11/10/14.
+//  Copyright (c) 2014 Davis Gossage. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
