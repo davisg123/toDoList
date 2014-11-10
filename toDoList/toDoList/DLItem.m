d7e75b7ce2d166645391922795c0eb134b3b1e7d
Davis Gossage <dcg13@duke.edu>
2014-11-10 15:27:56 -0500

    Our custom object uses coding to allow it to be saved as data

--- toDoList/toDoList/DLItem.m
+++ toDoList/toDoList/DLItem.m
@@ -10,4 +10,22 @@
 
 @implementation DLItem
 
+- (id)initWithCoder:(NSCoder *)coder {
+    self = [super init];
+    if (self) {
+        self.itemDescription = [coder decodeObjectForKey:@"itemDescription"];
+        self.icon = [UIImage imageWithData:[coder decodeObjectForKey:@"icon"]];
+        self.backgroundColor = [coder decodeObjectForKey:@"backgroundColor"];
+        self.itemTitle = [coder decodeObjectForKey:@"itemTitle"];
+    }
+    return self;
+}
+
+- (void)encodeWithCoder:(NSCoder *)coder {
+    [coder encodeObject:self.itemDescription forKey:@"itemDescription"];
+    [coder encodeObject:UIImagePNGRepresentation(self.icon) forKey:@"icon"];
+    [coder encodeObject:self.backgroundColor forKey:@"backgroundColor"];
+    [coder encodeObject:self.itemTitle forKey:@"itemTitle"];
+}
+
 @end
